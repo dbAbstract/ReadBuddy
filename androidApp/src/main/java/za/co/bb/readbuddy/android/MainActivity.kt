6b8b413d22043f30ab79ac8e02b0f243cae22fa0
taki.enari <taqi.enari@rakuten.com>
2023-12-26 00:08:34 +0200

    Refactor UI

--- androidApp/src/main/java/za/co/bb/readbuddy/android/MainActivity.kt
+++ androidApp/src/main/java/za/co/bb/readbuddy/android/MainActivity.kt
@@ -3,6 +3,7 @@ package za.co.bb.readbuddy.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -12,28 +13,20 @@ import androidx.compose.ui.tooling.preview.Preview
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
+        setThemedContent {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Text(text = "")
             }
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+private fun ComponentActivity.setThemedContent(
+    content: @Composable () -> Unit
+) {
+    setContent {
+        MyApplicationTheme {
+            content()
+        }
     }
 }
