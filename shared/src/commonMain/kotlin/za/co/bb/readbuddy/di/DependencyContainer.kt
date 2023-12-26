d7d012eb36f87432d02cca87f17f68b64a11a89f
taki.enari <taqi.enari@rakuten.com>
2023-12-26 11:38:38 +0200

    Add shared DI container

--- shared/src/commonMain/kotlin/za/co/bb/readbuddy/di/DependencyContainer.kt
+++ shared/src/commonMain/kotlin/za/co/bb/readbuddy/di/DependencyContainer.kt
@@ -2,9 +2,14 @@ package za.co.bb.readbuddy.di
 
 import za.co.bb.books_data.di.BooksDependencyContainer
 import za.co.bb.books_domain.repository.BooksRepository
+import za.co.bb.readbuddy.auth.AuthNetworkRepository
 
 object DependencyContainer {
     val booksRepository: BooksRepository by lazy {
         BooksDependencyContainer.booksRepository
+    }
+
+    val authNetworkRepository: AuthNetworkRepository by lazy {
+        AuthNetworkRepository()
     }
 }
