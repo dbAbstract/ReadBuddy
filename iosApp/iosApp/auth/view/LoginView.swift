3bc4365e15dbbfd67b7b09339facb68ce62171cf
taki.enari <taqi.enari@rakuten.com>
2023-12-26 11:41:15 +0200

    Create login view file in iOS

---
+++ iosApp/iosApp/auth/view/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  iosApp
+//
+//  Created by Enari, Taki | Taki | MDSD on 2023/12/26.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
