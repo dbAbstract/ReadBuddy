666fa98f26d85419bd9b544a057dfe7461cca0fc
taki.enari <taqi.enari@rakuten.com>
2023-12-27 18:43:04 +0200

    Update naming in schema files

--- books-data/src/commonMain/kotlin/com/arcanium/books_data/datasource/database/genre/GenreDao.kt
+++ books-data/src/commonMain/kotlin/com/arcanium/books_data/datasource/database/genre/GenreDao.kt
@@ -1,14 +1,10 @@
 package com.arcanium.books_data.datasource.database.genre
 
-import com.arcanium.books_data.datasource.database.genre.GenreDatabaseDriverFactory
 import com.arcanium.readybuddy.db.BooksDatabase
 
 internal class GenreDao(
     private val genreDatabaseDriverFactory: GenreDatabaseDriverFactory
 ) {
-    private val genreDatabase = BooksDatabase(genreDatabaseDriverFactory.createDriver())
+    private val bookDatabase = BooksDatabase(genreDatabaseDriverFactory.createDriver())
 
-    fun getGenre() {
-//        genreDatabase.
-    }
 }
