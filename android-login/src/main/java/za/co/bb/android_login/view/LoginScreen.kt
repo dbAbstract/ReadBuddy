f2e39cbecc3b1ebfd991382324316288b44b6d92
taki.enari <taqi.enari@rakuten.com>
2023-12-28 22:21:34 +0200

    Add new login module

---
+++ android-login/src/main/java/za/co/bb/android_login/view/LoginScreen.kt
@@ -0,0 +1,18 @@
+package za.co.bb.android_login.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+internal fun LoginScreen() {
+    Column {
+        
+    }
+}
+
+@Preview
+@Composable
+private fun LoginScreenPreview() {
+    
+}
